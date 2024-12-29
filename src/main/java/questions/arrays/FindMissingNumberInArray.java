17ba0b936cf91faad0e4bebd102ebd49936cfce0
sxmedh <sumedh11.sg@gmail.com>
2024-12-29 17:52:06 +0530

    commit

--- src/main/java/questions/arrays/FindMissingNumberInArray.java
+++ src/main/java/questions/arrays/FindMissingNumberInArray.java
@@ -2,10 +2,6 @@ package questions.arrays;
 
 public class FindMissingNumberInArray {
 
-    static int sumTillN(int n) {
-        return (n * (n + 1) / 2);
-    }
-
     static int findMissingNumberInArray(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] != i + 1) return i + 1;
